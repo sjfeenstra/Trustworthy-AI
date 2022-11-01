5929cd09597483fb043c25dbeac63bfc2cadd208
sjfeenstra <33319216+sjfeenstra@users.noreply.github.com>
2022-11-01 08:57:37 +0100

    save files

--- test onnxruntime.py
+++ test onnxruntime.py
@@ -14,17 +14,27 @@ output_name = session.get_outputs()[0].name
 
 data = np.load('data/numpy.npz')
 
+datasize = data['inputImgs'].shape[0]
+
 inputImgs_data = data['inputImgs']
 bigInputImgs_data = data['bigInputImgs']
 desire_data = data['desire']
 trafficConvention_data = data['trafficConvention']
 initialState_data = data['initialState']
 
-result = session.run([output_name], {input_imgs: np.vsplit(inputImgs_data, 198)[0],
-                                     big_input_imgs: np.vsplit(bigInputImgs_data, 198)[0],
-                                     desire: np.vsplit(desire_data, 198)[0],
-                                     traffic_convention: np.vsplit(trafficConvention_data, 198)[0],
-                                     initial_state: np.vsplit(initialState_data, 198)[0]
-                                     })
+res = np.empty([])
 
-res = np.array(result)
+for x in range(datasize):
+    result = session.run([output_name], {input_imgs: np.vsplit(inputImgs_data, datasize)[x],
+                                         big_input_imgs: np.vsplit(bigInputImgs_data, datasize)[x],
+                                         desire: np.vsplit(desire_data, datasize)[x],
+                                         traffic_convention: np.vsplit(trafficConvention_data, datasize)[x],
+                                         initial_state: np.vsplit(initialState_data, datasize)[x]
+                                         })
+    result = result[0]
+    if "6524" in str(res.shape):
+        res = np.concatenate((res, result), axis=0)
+    else:
+        res = result
+
+print(res)
